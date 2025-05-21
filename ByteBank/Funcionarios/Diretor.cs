0b02f6751ea317812634d56507a775970c75600f
Guilherme Meira <guilherme.meira@buhlergroup.com>
2025-05-21 11:32:19 -0300

    Backup

---
+++ ByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    internal class Diretor
+    {
+        //Classes 
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        //Metodo retornavel 
+        public double GetBonificacao()
+        {
+            return Salario;
+        }
+    }
+}
+
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -11,16 +11,23 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Funcionario carlos = new Funcionario(1);
+            Funcionario carlos = new Funcionario(0);
             carlos.Nome = "Carlos";
             carlos.CPF = "2688598892";
             carlos.Salario = 2000;
 
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "79926865584";
+            roberta.Salario = 5000;
+
             Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
             Console.ReadLine();
-
-
         }
     }
 }
