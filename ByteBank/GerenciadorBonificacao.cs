59026c2885d9e6bd66de8573e6c271d14a71f394
Guilherme Meira <guilherme.meira@buhlergroup.com>
2025-05-22 11:24:16 -0300

    Classe e Sobrecarga

---
+++ ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+    public class GerenciadorBonificacao
+    {
+        public double _totalBonificacao;
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+        public void Registrar(Diretor diretor)
+        {
+            _totalBonificacao += diretor.GetBonificacao();
+        }
+        public double GetBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -11,21 +11,27 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
             Funcionario carlos = new Funcionario(0);
             carlos.Nome = "Carlos";
             carlos.CPF = "2688598892";
             carlos.Salario = 2000;
+            gerenciador.Registrar(carlos);
 
             Diretor roberta = new Diretor();
             roberta.Nome = "Roberta";
             roberta.CPF = "79926865584";
             roberta.Salario = 5000;
+            gerenciador.Registrar(roberta);
 
             Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
 
             Console.WriteLine(roberta.Nome);
             Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetBonificacao());
 
             Console.ReadLine();
         }
